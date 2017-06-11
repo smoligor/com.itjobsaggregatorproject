02e7ed3205d0c88850c8711c9243157d435ce505
smoligor <smoligor@ukr.net>
2017-06-11 17:32:27 +0300

    added get random job method to controller

--- src/main/java/com/itjobsaggregatorproject/Controller/MainPageController.java
+++ src/main/java/com/itjobsaggregatorproject/Controller/MainPageController.java
@@ -3,6 +3,7 @@ package com.itjobsaggregatorproject.Controller;
 import com.itjobsaggregatorproject.Entity.Job;
 import com.itjobsaggregatorproject.Service.JobsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,18 +19,18 @@ import static org.springframework.http.HttpStatus.OK;
 public class MainPageController {
     @Autowired
     JobsService jobsService;
-
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/all")
     @ResponseStatus(OK)
     public List<Job> getJobs() {
         return jobsService.getAll();
-    }
 
+    }
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/random")
     @ResponseStatus(OK)
     public Job getRandomJob() {
         List<Job> jobs = jobsService.getAll();
         return jobs.get(new Random().nextInt(jobs.size()));
-
     }
 }
