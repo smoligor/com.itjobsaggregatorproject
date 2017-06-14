d4162304c1d3f42d619f81cc9da51472785f3bf9
smoligor <smoligor@ukr.net>
2017-06-14 16:00:00 +0300

    minor fix

--- src/test/java/com/itjobsaggregatorproject/DemoApplicationTests.java
+++ src/test/java/com/itjobsaggregatorproject/DemoApplicationTests.java
@@ -5,12 +5,17 @@ import com.itjobsaggregatorproject.service.JobService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static java.lang.Thread.sleep;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+
+    @Qualifier("workUa")
     @Autowired
     JobParser jobParser;
     @Autowired
